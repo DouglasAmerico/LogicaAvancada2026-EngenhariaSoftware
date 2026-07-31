b7b6193c8103b100d1cbec459f63e2bb645f6c8c
Douglas Americo <douglas.alex.1212@gmail.com>
2026-07-30 22:25:20 -0300

    +1

--- src/function.ts
+++ src/function.ts
@@ -1,2 +1,18 @@
 let recebe : string|null; 
 let numero1 : number =0, numero2: number=0;
+
+const validaSeNull = (valor:string):boolean => 
+    (valor != null) ? true:false;
+
+const validaSeVazio = (valor:string):boolean =>
+    (valor != "") ? true:false;
+
+const validaSeNumeroValido = (valor:string):boolean =>
+    (!isNaN(Number(valor))) ? true:false;
+
+const validaSeValorENumero = (valor : string): boolean =>
+    (!
+        (validaSeNumeroValido(valor))&&
+        (validaSeVazio(valor))       &&
+        (validaSeNull(valor))
+    ) ? true : false;
